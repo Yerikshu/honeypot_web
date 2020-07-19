3d5fc8b81fd2da8671d698698ed4f1ed7496bf6c
yerikyu <19802021029@139.com>
2020-07-19 21:03:23 +0800

    host

--- model/host.py
+++ model/host.py
@@ -3,8 +3,7 @@
 # @Author   : yerikyu
 # @Date     : 7/19/20
 # @Purpose  : 蜜罐主机状态表
-from conf.db import Base
-
+from conf.db import Base, engine
 from sqlalchemy import Column, String, TIMESTAMP, UniqueConstraint
 
 
@@ -19,9 +18,7 @@ class host(Base):
 
     __table_args__ = (
         # 设置联合唯一索引
-        UniqueConstraint('hostname', 'ip', name='uix_hostname_ip'),
-        # 设置联合索引
-        # Index('uix_hostname_ip', 'hostname', 'ip'),
+        UniqueConstraint('hostname', 'ip', name='uix_hostname_ip')
     )
 
 
@@ -31,20 +28,3 @@ def init_db():
 
 def drop_db():
     Base.metadata.drop_all(engine)
-
-
-if __name__ == "__main__":
-    init_db()
-    print('create Host table')
-
-"""
-CREATE TABLE `Host` (
-	id VARCHAR(50) NOT NULL,
-	last_time TIMESTAMP NOT NULL,
-	hostname VARCHAR(50) NOT NULL,
-	ip VARCHAR(50) NOT NULL,
-	status VARCHAR(10) NOT NULL,
-	PRIMARY KEY (id),
-	CONSTRAINT uix_hostname_ip UNIQUE (hostname, ip)
-)
-"""
